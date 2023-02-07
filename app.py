8acbc6e7121aadc3806e27c948b149aa29a12f0a
YZWarren <98486603+YZWarren@users.noreply.github.com>
2023-02-06 23:47:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def hello():
     return "Welcome to GAN playground, please go to /generate to generate image"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
