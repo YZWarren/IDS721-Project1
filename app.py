1df4f4201d3b7b5f312130621d65581296eaa4f5
YZWarren <98486603+YZWarren@users.noreply.github.com>
2023-02-08 16:23:29 +0000

    calculator flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from flask import Flask, render_template
+from flask import request, Flask, render_template, jsonify
+import requests
+
 # import io
 # from acgan.ACGAN import *
 # import torch
@@ -37,20 +38,102 @@ app = Flask(__name__)
 # def refresh_generate():
 #     gen_imgs = generate()
 #     gen_imgs = gen_imgs / 2 + 0.5
-    
-#     plt.imshow(np.transpose(gen_imgs, (1, 2, 0)))
+
+#     plt.imshow(np.trrespose(gen_imgs, (1, 2, 0)))
 #     plt.axis('off')
-    
+
 #     data = io.BytesIO()
 #     plt.savefig(data, format='png')
 #     img_stream = base64.b64encode(data.getvalue()).decode()
 #     return render_template('generate.html',
 #                            img_stream=img_stream)
 
-@app.route('/')
+
+def evaluate_expr(stack):
+    if not stack or type(stack[-1]) == str:
+        stack.append(0)
+
+    last_operation = " "
+    lastNum = stack.pop()
+    res = lastNum
+    while stack and stack[-1] != ")":
+        operation = stack.pop()
+        if operation == "+":
+            last_operation = "+"
+            lastNum = stack.pop()
+            res += lastNum
+        elif operation == "-":
+            last_operation = "+"
+            lastNum = -stack.pop()
+            res += lastNum
+        elif operation == "*":
+            curr = int(stack.pop())
+            if last_operation == "+":
+                res = res - lastNum + lastNum * curr
+            else:
+                res = res * curr
+            last_operation = " "
+            lastNum = curr
+        elif operation == "/":
+            curr = int(stack.pop())
+            if last_operation == "+":
+                res = res - lastNum + lastNum / curr
+            else:
+                res /= curr
+            last_operation = " "
+            lastNum = curr
+
+    return res
+
+
+def calculator(s):
+    stack = []
+    n, operand = 0, 0
+
+    for i in range(len(s) - 1, -1, -1):
+        ch = s[i]
+        if ch.isdigit():
+            operand = (10**n * int(ch)) + operand
+            n += 1
+
+        elif ch != " ":
+            if n:
+                stack.append(operand)
+                n, operand = 0, 0
+
+            if ch == "(":
+                res = evaluate_expr(stack)
+                stack.pop()
+                stack.append(res)
+            else:
+                stack.append(ch)
+
+    if n:
+        stack.append(operand)
+
+    return evaluate_expr(stack)
+
+
+@app.route("/")
 def hello():
     """return an HTTP greeting and show code rain."""
-    return render_template('hello.html')
+    return render_template("hello.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+@app.route("/calculate")
+def calculate_home():
+
+    return "Calculator that supports +, -, *, /, (, and ).Usage: /calculate/(1+2)*3\n\r"
+
+@app.route("/calculate/<expression>")
+def calculate_expr(expression):
+    
+    try:
+        result = calculator(str(expression))
+    except:
+        result = "Inputting expression illegal, try again"
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=False)
--- test_app.py
+++ test_app.py
@@ -1,7 +1,9 @@
 # from app import app
 
+
 def test_hello_world():
     assert True
+
 
 def test_chatgpt():
     assert True
